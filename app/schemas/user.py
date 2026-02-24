70362a66be57fc3868de00d4c039a6af108fff3a
Nanditha5244 <nandithanandu5244@gmail.com>
2026-02-24 20:02:35 +0530

    Email and password  Validation added

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,13 +1,44 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
+import re
+
 
 class UserCreate(BaseModel):
-    email: EmailStr
+    email: str
     password: str
     role: str = "user"
+
+    @field_validator("email")
+    @classmethod
+    def validate_gmail(cls, value):
+        if not value.endswith("@gmail.com"):
+            raise ValueError("Email must end with @gmail.com")
+        return value
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Password must contain at least one uppercase letter")
+
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Password must contain at least one lowercase letter")
+
+        if not re.search(r"\d", value):
+            raise ValueError("Password must contain at least one number")
+
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Password must contain at least one special character")
+
+        return value
+
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
 
 class TokenResponse(BaseModel):
     access_token: str
